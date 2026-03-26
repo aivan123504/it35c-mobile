679d5137d14c2783e1eb0c7fbde161cece8c4c8b
aivan123504 <20232309@nbsc.edu.ph>
2026-03-26 15:44:27 +0800

    adding components

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IonButton } from '@ionic/react';
+
+function Example() {
+  return (
+    <>
+      <IonButton expand="block">Block</IonButton>
+      <IonButton expand="full">Full</IonButton>
+    </>
+  );
+}
+export default Example;
