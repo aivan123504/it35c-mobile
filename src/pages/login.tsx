03d2e010a4602de958a70294556fd3d56c07a943
aivan123504 <20232309@nbsc.edu.ph>
2026-03-30 15:54:30 +0800

    add login

---
+++ src/pages/login.tsx
@@ -0,0 +1,32 @@
+        import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonRouter} from '@ionic/react';
+
+        const login: React.FC = () => {
+            const navigation = useIonRouter ();
+
+const dologin = () => {
+    navigation.push('/app', 'forward', 'replace');
+}
+
+
+
+              return (
+
+                <IonPage>
+                    <IonHeader>
+                        <IonToolbar>
+                            <IonTitle>Login </IonTitle>
+                        </IonToolbar>
+                          </IonHeader>
+                          
+                          
+                                          <IonContent className="ion-padding">
+                                            <IonButton expand="full" onClick={() => dologin()}>
+                                                login
+                                            </IonButton>
+                                          </IonContent>
+                                </IonPage>
+                              );
+                            }
+                
+                export default login;
+                    
---
+++ src/pages/page2.tsx
@@ -0,0 +1,15 @@
+import { IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+const Menu: React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>
+                        page2
+                    </IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </IonPage>
+    )
+};
+export default page2;
